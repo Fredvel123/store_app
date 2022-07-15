1622b1857e17362cbc45cd33b354341a301d4cbb
freddy velarde <freddyvelarde59#gmail.com>
2022-07-15 12:32:32 -0400

    improving install.sh file

--- install.sh
+++ install.sh
@@ -3,22 +3,28 @@
 # installing project with yarn
 echo "What do you prefer? just type yarn or npm"
 read npm_package 
-
+if [ $npm_package == 'yarn' ] || [ $npm_package == 'npm' ]
+then 
+echo " ----- Installing packages with $npm_package ---- "
+sleep 3
 cd client/
-echo $npm_package
-echo "Installing React Packages"
+
+echo "**** ALERT: Installing React Packages ****"
 $npm_package install
-echo "React installed"
+echo "----- React installed"
 
 sleep 2
 
-echo "Installing Express Packages"
+echo "**** ALERT: Installing Express Packages ****"
 
 cd server/
 
 $npm_package install
-echo "Express project installed"
+echo "----- ALERT: Express project installed"
 
 sleep 2 
 
-echo "Fullstack project installed"
+echo "----- ALERT: Fullstack project installed"
+else 
+echo 'WARNING: Only use npm or yarn package manager'
+fi
