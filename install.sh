6ea72bb5b998fe421f4cf61618243acd8b51434e
freddy velarde <freddyvelarde59#gmail.com>
2022-07-15 12:55:27 -0400

    install.sh bug fixed

--- install.sh
+++ install.sh
@@ -6,24 +6,22 @@ read npm_package
 if [ $npm_package == 'yarn' ] || [ $npm_package == 'npm' ]
 then 
 echo " ----- Installing packages with $npm_package ---- "
+
 sleep 3
-cd client/
-
-echo "**** ALERT: Installing React Packages ****"
-$npm_package install
-echo "----- React installed"
-
-sleep 2
-
 echo "**** ALERT: Installing Express Packages ****"
-
+sleep 2
 cd server/
-
 $npm_package install
 echo "----- ALERT: Express project installed"
 
-sleep 2 
+sleep 3
 
+echo "**** ALERT: Installing React Packages ****"
+cd ../client/
+$npm_package install
+echo "----- React installed"
+
+sleep 2 
 echo "----- ALERT: Fullstack project installed"
 else 
 echo 'WARNING: Only use npm or yarn package manager'
